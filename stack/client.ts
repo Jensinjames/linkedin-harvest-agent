0a5c35b62a91ac5a5d455a330f728a73e01d4624
Jensinjames <jensinjames@yahoo.com>
2025-07-23 13:14:40 -0500

    "modified stack/client.ts"

--- stack/client.ts
+++ stack/client.ts
@@ -1,8 +1,10 @@
 import { StackClientApp } from "@stackframe/js";
+import { env } from "node:process";
 
 export const stackClientApp = new StackClientApp({
   tokenStore: "cookie",
 
   // get your Stack Auth API keys from https://app.stack-auth.com and store them in a safe place (eg. environment variables)
-  publishableClientKey: INSERT_YOUR_PUBLISHABLE_CLIENT_KEY_HERE,
+  // get your Stack Auth API keys from https://app.stack-auth.com and store them in a safe place (eg. environment variables)
+  publishableClientKey: env.process = 'NEXT_PUBLIC_STACK_PUBLISHABLE_CLIENT_KEY',
 });
